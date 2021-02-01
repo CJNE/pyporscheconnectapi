46d019e7a1117f233b2aa7187a3ecfc62991df68
Johan Isacsson <johan@generatorhallen.se>
2021-02-01 16:17:11 +0100

    Version 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="pyporscheconnectapi",
-    version="0.0.3",
+    version="0.0.4",
     author="Johan Isaksson",
     author_email="johan@generatorhallen.se",
     description="Python wrapper for communicating with Porsche Connect API.",
