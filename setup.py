3da103a5f3478a55f72b98a1d010bd383df2ac60
Johan Isacsson <johan@generatorhallen.se>
2023-01-22 12:11:28 +0100

    Fixed privacy mode condition

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="pyporscheconnectapi",
-    version="0.0.24",
+    version="0.0.25",
     author="Johan Isaksson",
     author_email="johan@generatorhallen.se",
     description="Python library and CLI for communicating with Porsche Connect API.",
