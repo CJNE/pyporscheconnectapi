ae42d3aca1d2b212c7bd61adeb30e6d3fd98f3c3
Johan Isacsson <johan@generatorhallen.se>
2023-04-23 12:35:15 +0200

    Packaging fixes, version 0.0.26

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from setuptools import setup
 
 setup(
     name="pyporscheconnectapi",
-    version="0.0.25",
+    version="0.0.26",
     author="Johan Isaksson",
     author_email="johan@generatorhallen.se",
     description="Python library and CLI for communicating with Porsche Connect API.",
+    long_description=open("README.md", 'r').read(),
+    long_description_content_type='text/markdown',
     include_package_data=True,
     url="https://github.com/cjne/pyporscheconnectapi",
     license="MIT",
