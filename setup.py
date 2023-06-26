35872b56089feb93f8e0aa1e6c7a4b81b89cb1d0
Fredrik Ljunggren <fredrik@kirei.se>
2023-06-26 15:24:20 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="pyporscheconnectapi",
-    version="0.1.3",
+    version="0.1.4",
     author="Johan Isaksson",
     author_email="johan@generatorhallen.se",
     description="Python library and CLI for communicating with Porsche Connect API.",
